68ab95327820ae945a4af07dd820070d82891107
AlTheOne <altheone.official@gmail.com>
2018-09-14 19:09:04 +0500

    Update v.0.6.9 SearchApp

--- searchApp/views.py
+++ searchApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
-from catalogApp.models import Books
+from django.shortcuts import redirect
+from catalogApp.models import Books, GenreBooks, TagsBooks, Currency
 from django.db.models import Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from itertools import chain
@@ -13,48 +14,142 @@ class MyPagination():
 		# args = ожидаем QuerySet
 		# Пагинация по результатам
 		data = {}
-		data['paginator'] = Paginator(*args, nmb)
+		data['paginator'] = Paginator(self.QS, nmb)
 		page = self.request.GET.get('page')
 
+
 		try:
-			data['resultPage'] = data['paginator'].page(page)
+			self.QS = data['paginator'].page(page)
 		except PageNotAnInteger:
 			# If page is not an integer, deliver first page.
-			data['resultPage'] = data['paginator'].page(1)
+			self.QS = data['paginator'].page(1)
 		except EmptyPage:
 			# If page is out of range (e.g. 9999), deliver last page of results.
-			data['resultPage'] = data['paginator'].page(data['paginator'].num_pages)
+			self.QS = data['paginator'].page(data['paginator'].num_pages)
 
-		return (data['resultPage'], data['paginator'])
+		return data['paginator']
 
 
-class SearchPage(View, MyPagination):
-	TEMPLATE = 'catalogApp/catalog-main.html'
+class InitFilter(object):
+	# Инициализация шаблона
+	def init_view(self):
+		if 'type_view' in self.request.COOKIES:
+			if self.request.COOKIES.get('type_view') == 'list':
+				self.TEMPLATES = 'catalogApp/catalog-main-list.html'
+		pass
+
+	# Инициализация сортировки
+	def init_filter(self):
+		if 'type_filter' in self.request.COOKIES:
+			if self.request.COOKIES.get('type_filter') == '2':
+				self.FILTER_SORT = 2
+				self.QS = self.QS.order_by('-created')
+			elif self.request.COOKIES.get('type_filter') == '3':
+				self.FILTER_SORT = 3
+				self.QS = self.QS.order_by('-title')
+			elif self.request.COOKIES.get('type_filter') == '4':
+				self.FILTER_SORT = 4
+				self.QS = self.QS.order_by('title')
+			elif self.request.COOKIES.get('type_filter') == '5':
+				self.FILTER_SORT = 5
+				self.QS = self.QS.order_by('-date')
+			elif self.request.COOKIES.get('type_filter') == '6':
+				self.FILTER_SORT = 6
+				self.QS = self.QS.order_by('date')
+			elif self.request.COOKIES.get('type_filter') == '7':
+				self.FILTER_SORT = 7
+				self.QS = self.QS.order_by('-price')
+			elif self.request.COOKIES.get('type_filter') == '8':
+				self.FILTER_SORT = 8
+				self.QS = self.QS.order_by('price')
+			else:
+				self.QS = self.QS.order_by('created')
+		pass
+
+	# Инициализация цен
+	def init_currency(self):
+		if 'type_currency' in self.request.COOKIES:
+			try:
+				self.CURRENCY = Currency.objects.get(slug=self.request.COOKIES.get('type_currency'))
+	
+				for n in range(len(self.QS)):
+					self.QS[n].price =  round(self.QS[n].price * self.CURRENCY.quota, 2)
+					self.QS[n].price_discount = round(self.QS[n].price_discount * self.CURRENCY.quota, 2)
+
+			except Currency.DoesNotExist:
+				pass
+		pass
+
+	# Инициализация по меткам и жанрам
+	def init_mainfilter(self):
+		if 'type_genres' in self.request.COOKIES:
+			tg = self.request.COOKIES.get('type_genres')
+			genres = [int(i) for i in tg[1:-1].split(', ')]
+			self.FILTER_GENRES = genres
+			self.QS = self.QS.filter(genre__in=genres).distinct()
+
+		if 'type_tags' in self.request.COOKIES:
+			tt = self.request.COOKIES.get('type_tags')
+			tags = [int(i) for i in tt[1:-1].split(', ')]
+			self.FILTER_TAGS = tags
+			self.QS = self.QS.filter(tags__in=tags).distinct()
+		pass
+
+
+class SearchPage(View, MyPagination, InitFilter):
+	TEMPLATES = 'catalogApp/catalog-main.html'
+	QS = None
+
+	FILTER_GENRES = None
+	FILTER_TAGS = None
+	FILTER_SORT = None
+	CURRENCY = None
+
 	def post(self, *args, **kwargs):
 		data = {}
 		if self.request.POST.get('q') is not None:
-			data['q'] = self.request.POST.get('q')
-			
-			data['search'] = Books.objects.filter(
-				Q(title__icontains=data['q']) | 
-				Q(description__icontains=data['q'])
-				# Q(genre__icontains=data['q'])|
+			data['search_q'] = self.request.POST.get('q')
+
+			# Инициализация шаблона
+			self.init_view()
+
+			# Инициализация сортировки
+			self.init_filter()
+
+			self.QS = Books.objects.filter(
+				Q(title__icontains=data['search_q']) | 
+				Q(description__icontains=data['search_q'])
+		 		# Q(genre__icontains=data['q'])|
 				# Q(tags__icontains=data['q'])
 			).reverse()
 
-			data['q-word'] = data['q'].split(' ')
+			data['q-word'] = data['search_q'].split(' ')
 			if len(data['q-word']) > 1:
 				for q in data['q-word']:
 					data['obj'] = Books.objects.filter(
-						Q(title__icontains=data['q']) | 
-						Q(description__icontains=data['q'])
+						Q(title__icontains=data['search_q']) | 
+						Q(description__icontains=data['search_q'])
 						# Q(genre__icontains=data['q'])|
 						# Q(tags__icontains=data['q'])
 						).reverse()
-					data['search'] = list(chain(data['search'], data['obj']))
+					self.QS = list(chain(data['search'], data['obj']))
 
-			data['objects'], data['paginator'] = self.mainPaginator(20, data['search'])
-			data['search_q'] = self.request.POST.get('q')
-			return render(self.request, self.TEMPLATE, context=data)
+			data['genres'] = GenreBooks.objects.filter(is_active=True).order_by('title')
+			data['tags'] = TagsBooks.objects.filter(is_active=True).order_by('title')
+			data['currency'] = Currency.objects.filter(is_active=True).order_by('title')
+
+			# Инициализация категорий и меток
+			self.init_mainfilter()
+
+			# data['paginator'] = self.mainPaginator(2)
+
+			# Инициализация валют
+			self.init_currency()
+			data['objects'] = self.QS
+			data['get_genres'] = self.FILTER_GENRES
+			data['get_tags'] = self.FILTER_TAGS
+			data['get_sort'] = self.FILTER_SORT
+			data['rune'] = self.CURRENCY
+			return render(self.request, self.TEMPLATES, context=data)
 		else:
-			return render(self.request, self.TEMPLATE, context=data)
+			return redirect('/')
